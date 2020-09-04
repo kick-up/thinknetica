35d8c3f483e3547a825d73a655f09c9e4b377d97
Darkhan <darkhanbek89@gmail.com>
2020-09-05 00:00:44 +0600

    month_hash

---
+++ lesson2/month_hash.rb
@@ -0,0 +1,22 @@
+months  =  {
+    january: 31,
+    february: 28,
+    march: 31,
+    april: 30,
+    may: 31,
+    june: 30,
+    july: 31,
+    august: 31,
+    september: 30,
+    october: 31,
+    november: 30,
+    december: 31
+}
+
+def days_count(months, days)
+  months.each do |month,  day|
+    puts month if day == days
+  end
+end
+
+days_count(months,  30)
