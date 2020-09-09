9dbbb2ba7cd53b7baffc91f585f7cb6e92a1cc70
Darkhan <darkhanbek89@gmail.com>
2020-09-09 13:40:00 +0600

    Route

---
+++ lesson3/route.rb
@@ -0,0 +1,28 @@
+#Класс Route (Маршрут):
+#Имеет начальную и конечную станцию, а также список промежуточных станций. 
+#Начальная и конечная станции указываютсся при создании маршрута, 
+
+class Route
+  attr_reader :start, :transit, :end
+  def initialize(start, end)
+    @start  = start
+    @end  = end
+    @transit  = []
+  end
+
+# промежуточные могут добавляться между ними.
+#Может добавлять промежуточную станцию в список
+  def add_transit(station)
+    @transit << station
+  end
+
+#Может удалять промежуточную станцию из списка
+  def delete_station(station)
+    @transit.delete(station)
+  end
+
+#Может выводить список всех станций по-порядку от начальной до конечной
+  def station
+    [@start, @transit, @end]
+  end
+end
