7a678de0f74ef9a49e0e3a7f1e9563d51af3d1a2
Darkhan <darkhanbek89@gmail.com>
2020-09-21 23:28:29 +0600

    route

---
+++ lesson5/route.rb
@@ -0,0 +1,26 @@
+require_relative '../instance_counter'
+
+class Route
+  attr_reader :origin, :transit_stations, :destination, :stations
+  def initialize(origin,destination)
+    @origin = origin
+    @destination  = destination
+    @transit_stations = []
+  end
+
+  def add_transit_station(station)
+    @transit_stations <<  station
+  end
+
+  def delete_transit_station(station)
+    @transit_stations.delete(station)
+  end
+
+  def stations
+    [@origin, @transit_stations,  @destination]
+  end
+
+  def to_s
+    [stations.first, [@transit_stations], stations.last].join(' - ')
+  end
+end
