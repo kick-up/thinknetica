44986cef93a4961e3cb6664c5fd0e622ab8bfd8d
Darkhan <darkhanbek89@gmail.com>
2020-09-25 23:33:47 +0600

    lesson7

---
+++ lesson7/train/passenger_train.rb
@@ -0,0 +1,14 @@
+require_relative '../instance_counter'
+require_relative '../maker'
+
+class PassengerTrain  < Train
+  def initialize(number)
+    @type = "Passenger"
+    super
+  end
+
+  def attachable_carriage?(carriage)
+    carriage.is_a?(PassengerCarriage)
+  end
+
+end
